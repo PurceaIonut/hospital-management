924b1dd154bd014b225c0264d1184c182100c1e7
PurceaIonut <114928322+PurceaIonut@users.noreply.github.com>
2023-03-18 21:10:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ class Treatment(db.Model):
 #Register Admin
 @app.route('/register/admin', methods=['GET'])
 def admin():
-    admin = User(email= 'general_manager@example.com', password = 'insert password', role= 'General Manager')
+    admin = User(email= 'general_manager@example.com', password = 'password', role= 'General Manager')
     db.session.add(admin)
     db.session.commit()
     return 'Admin has been created'
